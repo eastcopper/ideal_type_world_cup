03e1ad3b40606b89456a24f0a42a05adb12b65bb
ldh7228 <ldh7228@gmail.com>
2021-06-02 16:54:35 +0900

    .

--- start.js
+++ start.js
@@ -1,4 +1,16 @@
-let round = 8;
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+
+let files = ["./asset/1.png",
+             "./asset/2.png",
+             "./asset/3.png",
+             "./asset/4.png",
+             "./asset/5.png",
+             "./asset/6.png",
+             "./asset/7.png",
+             "./asset/8.png"]; // 사진 배열
+
+let round = files.length;
 
 Array.prototype.shuffle = function () {
     let length = this.length;
@@ -10,16 +22,28 @@ Array.prototype.shuffle = function () {
 
         this[round] = this[index];
 
-        this[index] = temp;
+        this[index] = temp; // 배열 랜덤
     }
-    round /= 2;
     return this;
 }
 
-let array = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png']
+files.shuffle();
 
-console.log(array)
+let imgs = new Array();
+for (let i = 0; i<files.length; i++) {
+    imgs[i] = new Image();
+    imgs[i].src = files[i];// 이미지 미리 로딩
+}
 
-for(let i = 0; i < round / 2; i++) {
-    console.log(array[i])
+img1.src = imgs[0];
+img2.src = imgs[1];
+
+let next = 2;
+
+function change1(img) {
+    img1.src = imgs[next].src;
+    next++;
+    if(next == 9) {
+        next = 0;
+    }
 }
