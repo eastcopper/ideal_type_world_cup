5683fd29e042d36c6ac2148b723c71fa4e052022
ldh7228 <ldh7228@gmail.com>
2021-06-03 19:05:54 +0900

    Auto stash before merge of "master" and "origin/master"

--- start.js
+++ start.js
@@ -16,16 +16,12 @@ for (var i = 0; i < 8; i++) {
     Arr.push(0);
 }
 
-let sum = [];
-
-for (let i = 0; i < files.length; i++) {
-    sum[i] = files[i];
-}
-
-let round = files.length;
-
 Array.prototype.shuffle = function () {
     
+<<<<<<< Updated upstream
+=======
+let round = files.length;
+>>>>>>> Stashed changes
 
     while (round) {
         let index = Math.floor((round--) * Math.random());
@@ -36,9 +32,9 @@ Array.prototype.shuffle = function () {
 
         this[index] = temp; // 배열 랜덤
     }
+
     return this;
 }
-
 
 files.shuffle();
 
@@ -48,7 +44,7 @@ let imgs2 = new Array();
 
 for (let i = 0; i < files.length; i++) {
     imgs8[i] = new Image();
-    imgs8[i].src = files[i];// 이미지 미리 로딩
+    imgs8[i].src = files[i];// 이미지 로딩
 }
 
 let indexNum = 0;
@@ -67,6 +63,7 @@ function change(e) {
         }
         indexNum += 2;
 
+        console.log(Arr)
 
         if (indexNum >= 8) {
             for (let i = 0; i < 8; i++) {
@@ -79,20 +76,24 @@ function change(e) {
                     files.splice(i, 1); // 배열 제외
                 }
             }
-
-
+            for (let i = 0; i < 8; i++) {
+                console.log(files[i])
+            }
             indexNum = 0;
             roundNum = 4;
 
             
             for (let i = 0; i < 4; i++) {
                 imgs4[i] = new Image();
-                imgs4[i].src = files[i];// 이미지 미리 로딩
+                imgs4[i].src = files[i];// 이미지 로딩
             }
 
             for (let i = 0; i < 4; i++) {
-                console.log(files[i])
+                // console.log(files[i])
             }
+
+            files.shuffle();
+
             img1.src = imgs4[indexNum].src
             img2.src = imgs4[indexNum + 1].src
         }
@@ -107,39 +108,45 @@ function change(e) {
         } else {
             Arr[indexNum + 1]++;
         }
+
+        console.log(Arr)
         indexNum += 2;
 
 
         if (indexNum >= 4) {
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 8; i++) {
                 if (Arr[i] == 1) {
                     files[i] = Arr[i];
                 }
             }
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 8; i++) {
                 if (files[i] === 1) {
                     files.splice(i, 1); // 배열 제외
+                    Arr.splice(i, 1);
                 }
             }
 
-
             indexNum = 0;
             roundNum = 2;
-
             
             for (let i = 0; i < 2; i++) {
                 imgs2[i] = new Image();
-                imgs2[i].src = files[i];// 이미지 미리 로딩
+                imgs2[i].src = files[i];// 이미지 로딩
             }
+            
+            console.log(imgs2[0].src)
+            console.log(imgs2[1].src)
 
             img1.src = imgs2[indexNum].src
             img2.src = imgs2[indexNum + 1].src
 
         }
         else if (indexNum < 4) {
-            console.log(imgs4[2].src)
             img1.src = imgs4[indexNum].src;
             img2.src = imgs4[indexNum + 1].src;
         }
     }// 4강
+    else if (roundNum == 2) {
+        
+    }
 }
